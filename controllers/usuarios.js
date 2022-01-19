5fe2cb87d3f25c5948b1b42e0d2637e7e4949c9e
Andres Castano <andres@unlimitech.cloud>
2022-01-19 13:29:29 -0500

    npm start trying

--- controllers/usuarios.js
+++ controllers/usuarios.js
@@ -7,7 +7,7 @@ const { PromiseProvider } = require('mongoose');
 const usuariosGet = async(req = request, res = response) => {
     // const { id, nombre = null, apikey } = req.query;
     const { limite = 5 , desde = 0} = req.query;
-    const query = {estado:true}
+    const query = {estado:true};
     // const usuarios = await Usuario.find(query)
     //     .skip(  Number( desde ) )
     //     .limit( Number( limite ) )
@@ -21,7 +21,7 @@ const usuariosGet = async(req = request, res = response) => {
     //         .limit( Number( limite ) )
     // ])
 
-    const [total, usuarios] = await Promise.all([
+    const [ total, usuarios ] = await Promise.all([
         Usuario.countDocuments(query),
         Usuario.find(query)
             .skip(  Number( desde ) )
